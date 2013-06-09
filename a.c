038c64d1eaf06dd4eb367479b63cb643356b5a40
nofutrue <edge.dragon@gmail.com>
2013-06-09 16:16:49 +0800

    nice job

---
+++ a.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  a.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  06/09/2013 04:15:46 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Juezhong Long (lzj), juezhong.long@kunlun-inc.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%s\n", "Hello World");
+	return 0;
+}
